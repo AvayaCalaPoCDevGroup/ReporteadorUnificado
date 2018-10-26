8be16566e8db2fa7473bee4410c74bbde2b7c5f4
Alam García Roldán <garcia76@avaya.com>
2018-10-25 20:05:20 -0500

    Se agrega función CMS   -Grupo de Agentes     -Nivel de Servicio

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -398,4 +398,91 @@ $("#cms_atendidas_agentes-frm").submit(function(event) {
 });
 
 
+$("#cms_servicio_agentes-frm").submit(function(event) {
+  //stop submit the form, we will post it manually.
+  event.preventDefault();
+  // Get form
+  var form = $('#cms_servicio_agentes-frm')[0];
+  // Create an FormData object
+  var data = new FormData(form);
+  // disabled the submit button
+  $("#report1-frm-btn").prop("disabled", true);
+
+  $.ajax({
+    type: "POST",
+    enctype: 'multipart/form-data',
+    url: "php/cms_servicio_agentes.php",
+    data: data,
+    processData: false,
+    contentType: false,
+    cache: false,
+    timeout: 600000,
+    success: function(data) {
+      $("#report1-frm-btn").prop("disabled", false);
+      console.log(data);
+       var json = $.parseJSON(data)
+       console.log(json);
+       var variable  = json["0"].sl;
+       var variable  = parseFloat(variable);
+       console.log(variable);
+       var total = 100 - variable;
+
+
+
+
+      var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+      
+var pieChart       = new Chart(pieChartCanvas)
+var PieData        = [
+  {
+    value    : variable,
+    color    : '#256d0d',
+    highlight: '#256d0d',
+    label    : 'Contestadas'
+  },
+  {
+    value    : total,
+    color    : '#f56954',
+    highlight: '#f56954',
+    label    : 'No contestadas'
+  }
+]
+var pieOptions     = {
+  //Boolean - Whether we should show a stroke on each segment
+  segmentShowStroke    : true,
+  //String - The colour of each segment stroke
+  segmentStrokeColor   : '#fff',
+  //Number - The width of each segment stroke
+  segmentStrokeWidth   : 2,
+  //Number - The percentage of the chart that we cut out of the middle
+  percentageInnerCutout: 50, // This is 0 for Pie charts
+  //Number - Amount of animation steps
+  animationSteps       : 100,
+  //String - Animation easing effect
+  animationEasing      : 'easeOutBounce',
+  //Boolean - Whether we animate the rotation of the Doughnut
+  animateRotate        : true,
+  //Boolean - Whether we animate scaling the Doughnut from the centre
+  animateScale         : true,
+  //Boolean - whether to make the chart responsive to window resizing
+  responsive           : true,
+  // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+  maintainAspectRatio  : true,
+  //String - A legend template
+  legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+}
+//Create pie or douhnut chart
+// You can switch between pie and douhnut using the method below.
+pieChart.Doughnut(PieData, pieOptions)
+
+
+    },
+    error: function(e) {
+      console.log("ERROR : ", e);
+      $("#report1-frm-btn").prop("disabled", false);
+    }
+  });
+});
+
+
 });
